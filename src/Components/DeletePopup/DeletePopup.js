ac40302ed2b34157cc05cdb3c8fb7a2c81f17aab
Nirojan21 <nirojan.yoga@gmail.com>
2021-08-20 15:01:44 +0530

    delete prompt component added

---
+++ src/Components/DeletePopup/DeletePopup.js
@@ -0,0 +1,19 @@
+import classes from "./DeletePopup.module.css";
+import { ReactDOM } from "react-dom";
+
+const DeletePopup = (props) => {
+  return (
+    <>
+    <div className={classes.backdrop} onClick={props.hide}></div>
+      <div className={classes.container}>
+          <h2 className={classes.title}>Are You Sure to Delete ?</h2>
+          <div className={classes.buttons}>
+              <span className={classes.cancel} onClick={props.hide}>Cancel</span>
+              <button className={classes.deleteButton} onClick={props.onDelete}>Delete</button>
+          </div>
+      </div>
+    </>
+  );
+};
+
+export default DeletePopup;
