fc3ca5d894f6a6dbee925f3e3dd23985bbb52bb8
Nirojan21 <nirojan.yoga@gmail.com>
2021-09-06 23:43:26 +0530

    css changed

--- src/Pages/HelpDeskAdmin/HelpDeskAdmin.js
+++ src/Pages/HelpDeskAdmin/HelpDeskAdmin.js
@@ -3,16 +3,36 @@ import Details from "./Details";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import Loader from "../../Components/Loader/Loader";
+import SearchBar from "./SearchBar";
 
 const HelpDeskAdmin = () => {
   const [queries, setQueries] = useState([]);
+  const [updatedQueries, setUpdatedQueries] = useState([]);
   const [loaded, setLoaded] = useState(false);
+  const [empty, setEmpty] = useState(false);
+
+  const onSearch = (value) => {
+    if (!value.trim()) {
+      setEmpty(false);
+      setUpdatedQueries(queries);
+      return;
+    }
+
+    const updated = queries.filter((query) =>
+      query.username.toUpperCase().includes(value.trim().toUpperCase())
+    );
+    setUpdatedQueries(updated);
+    if (updated.length === 0) {
+      setEmpty(true);
+    }
+  };
 
   useEffect(() => {
     axios
       .get("http://localhost:5000/helpDesk/get_tickets")
       .then((res) => {
         setQueries(res.data);
+        setUpdatedQueries(res.data);
         setLoaded(true);
       })
       .catch((er) => {
@@ -24,10 +44,12 @@ const HelpDeskAdmin = () => {
       {loaded && queries && (
         <div className={classes.container}>
           <h2 className={classes.title}>E-Tickets</h2>
-          <hr className={classes.line}></hr>
-          {queries.map((row) => {
+          {/* <hr className={classes.line}></hr> */}
+          <SearchBar onSearch={onSearch} />
+          {updatedQueries.map((row) => {
             return <Details data={row} />;
           })}
+          {empty && <div className={classes.nomatch}>No match found !</div>}
         </div>
       )}
       {!loaded && (
@@ -35,7 +57,9 @@ const HelpDeskAdmin = () => {
           <Loader />
         </div>
       )}
-      {!queries && <div className={classes.no_data}>no tickets available !!</div>}
+      {!queries && (
+        <div className={classes.no_data}>No tickets available !!</div>
+      )}
     </>
   );
 };
