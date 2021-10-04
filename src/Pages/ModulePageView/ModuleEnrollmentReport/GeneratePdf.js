bf159babf87bf2cd2044cbb04dc1ad689cc4d580
Sayanthan <kathirsayanthan@gmail.com>
2021-10-04 13:19:46 +0530

    Generate Pdf finished

--- src/Pages/ModulePageView/ModuleEnrollmentReport/GeneratePdf.js
+++ src/Pages/ModulePageView/ModuleEnrollmentReport/GeneratePdf.js
@@ -1,42 +1,42 @@
-// import jsPDF from "jspdf";
-// import "jspdf-autotable";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
-// const GeneratePDF = (students, moduleId, Module) => {
-//   const doc = new jsPDF();
+const GeneratePDF = (students, moduleId, Module) => {
+  const doc = new jsPDF();
 
-//   const tableColumn = ["ID", "type", "Name", "Faculty"];
+  const tableColumn = ["ID", "type", "Name", "Faculty"];
 
-//   const tableRows = [];
+  const tableRows = [];
 
-//   students.forEach((row) => {
-//     const insightData = [row.ID, row.type, row.name, row.faculty];
+  students.forEach((row) => {
+    const insightData = [row.ID, row.type, row.name, row.faculty];
 
-//     tableRows.push(insightData);
-//   });
+    tableRows.push(insightData);
+  });
 
-//   Module.forEach((row1) => {
-//     const ModuleName = row1.Modulename;
-//     const ModuleCode = row1.ModuleCode;
-//     const EnrollmentsKey = row1.ModuleEnrollmentkey;
+  Module.forEach((row1) => {
+    const ModuleName = row1.Modulename;
+    const ModuleCode = row1.ModuleCode;
+    const EnrollmentsKey = row1.ModuleEnrollmentkey;
 
-//     doc.autoTable(tableColumn, tableRows, { startY: 50 }, { Theme: "grid" });
+    doc.autoTable(tableColumn, tableRows, { startY: 50 }, { Theme: "grid" });
 
-//     doc.setFontSize(15);
-//     doc.text(
-//       "Enrollment Report\n\nTotalEnrollment : " +
-//         students.length +
-//         "\tModuleName :" +
-//         ModuleName +
-//         "\nModuleCode : " +
-//         ModuleCode +
-//         "\tModuleEnrollmentkey :" +
-//         EnrollmentsKey,
-//       25,
-//       15
-//     );
+    doc.setFontSize(15);
+    doc.text(
+      "Enrollment Report\n\nTotalEnrollment : " +
+        students.length +
+        "\tModuleName :" +
+        ModuleName +
+        "\nModuleCode : " +
+        ModuleCode +
+        "\tModuleEnrollmentkey :" +
+        EnrollmentsKey,
+      25,
+      15
+    );
 
-//     doc.save(`enroll_insight_${moduleId}.pdf`);
-//   });
-// };
+    doc.save(`enroll_insight_${moduleId}.pdf`);
+  });
+};
 
-// export default GeneratePDF;
+export default GeneratePDF;
